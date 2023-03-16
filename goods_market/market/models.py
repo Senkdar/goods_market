8d0d023011dae419ab8d7dd7f50a8f962c77ba7b
Алексей Кринкин <42-80-67@mail.ru>
2023-03-16 23:30:55 +0300

    remove username

--- goods_market/market/models.py
+++ goods_market/market/models.py
@@ -1,10 +1,27 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.core.validators import RegexValidator
 from django.db import models
 
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
 class CustomUser(AbstractUser):
     """Модель пользователя"""
+    username = None
     last_name = models.CharField('Фамилия', max_length=256)
     first_name = models.CharField('Имя', max_length=256)
     patronymic = models.CharField(
@@ -12,9 +29,10 @@ class CustomUser(AbstractUser):
         max_length=256,
         blank=True,
         null=True
+
     )
     email = models.EmailField(unique=True)
-    password = models.CharField('Пароль', max_length=150)
+    password = models.CharField(max_length=150)
     phone_validator = RegexValidator(
             regex=r'^8-\d{3}-\d{3}-\d{2}-\d{2}$',
             message="Номер телефона должен быть в формате: '8-xxx-xxx-xx-xx'"
@@ -25,6 +43,12 @@ class CustomUser(AbstractUser):
         unique=True,
         validators=[phone_validator,]
     )
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['last_name', 'first_name',]
+
+    objects = CustomUserManager()
 
     def __str__(self):
         return (f'{self.first_name} {self.last_name}')
